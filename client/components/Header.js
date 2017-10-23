6c09208ceefa5f41fcd24f537ade16f9ebbb0d5b
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-23 18:11:00 +0700

    header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import query from "../queries/current_user";
+import { Link } from "react-router";
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+    // if still loading, shows nothing
+    if (loading) {
+      return <div />;
+    }
+    if (user) {
+      // if user is not null (logged in)
+      return <div>Logout</div>;
+    } else {
+      return (
+        // if user is null (logged out)
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
   render() {
-    console.log(this.props.data);
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link className="brand-logo left">Home</Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
