b0344659e43d7bc740bf062ce534ae4d02ac3281
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-23 14:01:38 +0700

    authentication check

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,17 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require("./user_type");
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    dummyField: { type: GraphQLID }
+    // to check if a user has logged in or not
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        return req.user;
+      }
+    }
   }
 });
 
