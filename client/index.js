49b6fd22471c4198a665171a6fcbe3796130d76d
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-23 14:28:26 +0700

    wires apollo with react

--- client/index.js
+++ client/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  // with assumption that every type has an id,
+  // and everytime we fetch data, we have to request the id as well
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
